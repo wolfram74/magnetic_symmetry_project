4d1a6955c6b9c4487c7a4bc1d0cbcd041443aea8
wolfram74 <phaugen@gmail.com>
2020-01-30 13:23:34 -0600

    interesting results of plots

--- src/numerical/alpha_sweeping/dynamics_plots.py
+++ src/numerical/alpha_sweeping/dynamics_plots.py
@@ -15,16 +15,16 @@ def shift_outers(state_in, epsi=10.**-2):
 
 def oscilation_path(alpha):
     magnets.load_state(alpha)
-    del_state = shift_outers(magnets.state)
+    del_state = shift_outers(magnets.state, 10.**-3)
     state_0 = numpy.array(magnets.state)
     magnets.state+=del_state
     path = [(0, del_state[:7])]
     E_0 = magnets.total_PE()+magnets.total_KE()
     print(magnets.gamma)
-    while magnets.elapsed < 5:
+    while magnets.elapsed < 30:
         magnets.advance_in_time()
         E_t = magnets.total_PE()+magnets.total_KE()
-        print(E_t-E_0)
+        print(abs((E_t-E_0)/E_0))
         delta = magnets.state[:7]-state_0[:7]
         path.append((magnets.elapsed, tuple(delta)))
     return path
@@ -37,6 +37,6 @@ def plot_path(path, alpha):
     pyplot.savefig('%d-alph_%s.png' % (time.time(), alpha))
 
 if __name__=='__main__':
-    alph = 0.
+    alph = .5
     path = oscilation_path(alph)
-    # plot_path(path, alph)
+    plot_path(path, alph)
