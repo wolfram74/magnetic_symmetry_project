52f4e9d7cca5c35db092f8ea36f07577474ac875
wolfram74 <phaugen@gmail.com>
2020-04-26 13:30:32 -0500

    cached eigen mode values, will see if plots are faster

--- src/numerical/alpha_sweeping/sweeper.py
+++ src/numerical/alpha_sweeping/sweeper.py
@@ -55,10 +55,46 @@ def goin_down_states(magnets):
             running = False
     return equilibria
 
+def eigen_modes_poly(poly=True):
+    magnets = system.System()
+    if poly:
+        magnets.load_state(.01)
+        address_template = './saved_eigenmodes/poly_%d_mode.txt'
+        amax = 2.48
+        looping = lambda alph: alph<amax
+        shift = lambda mags: mags.load_state(mags.alpha+.011)
+        mode_find = lambda mags: mags.labeled_spectra()
+
+    if not poly:
+        magnets.load_state(3.5)
+        address_template = './saved_eigenmodes/mono_%d_mode.txt'
+        amin=1.15
+        looping = lambda alph: alph>amin
+        shift = lambda mags: mags.load_state(mags.alpha-.009, down=mono_specific)
+        mode_find = lambda mags: mags.labeled_spectra_mono()
+
+    destinations = []
+    eigen_states = mode_find(magnets)
+    for key in eigen_states.keys():
+        destinations.append(open(address_template%key, 'w'))
+
+
+    #save data is alpha, w^2, phi vector
+    line_template = '%.9f,'+'%.9f,'+'%.9f,'*7+'\n'
+    while looping(magnets.alpha):
+        eigen_states = mode_find(magnets)
+        for key in eigen_states.keys():
+            data = [magnets.alpha, eigen_states[key][0]]+list(eigen_states[key][1])
+            destinations[key-1].write(line_template % tuple(data))
+        mono_specific = magnets.alpha < 2.5 and not poly
+        shift(magnets)
+
 
 if __name__ =='__main__':
     # gen_text()
-    gen_down_text()
+    # gen_down_text()
+    # eigen_modes_poly()
+    eigen_modes_poly(poly=False)
 
 
 # for ind in range(7):
